6cd45e392c3c9090c889d5c7ac80e7054cd47878
leonischu <nischal.201423@ncit.edu.np>
2026-03-09 15:08:07 +0545

    ORDER BY

---
+++ SQLQuery3.sql
@@ -0,0 +1,19 @@
+SELECT
+
+    first_name,
+    last_name
+FROM
+    sales.customers
+ORDER BY
+    first_name DESC;
+
+
+	SELECT
+	city,
+    first_name,
+    last_name
+FROM
+    sales.customers
+ORDER BY
+city DESC,
+    first_name ASC;
