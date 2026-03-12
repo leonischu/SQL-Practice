44318f202bd3d7021e20d45b996d57bef774c145
leonischu <nischal.201423@ncit.edu.np>
2026-03-12 15:21:26 +0545

    Selecting a database from query

--- SQLQuery1testsql.sql
+++ SQLQuery1testsql.sql
@@ -3,3 +3,13 @@
 
 EXEC sp_databases
 SELECT name FROM sys.databases
+
+
+--Creating a database 
+
+ CREATE DATABASE school_db
+
+
+ --selecting a DB
+USE school_db 
+SELECT DB_NAME()  -- it checks which database am i using 
